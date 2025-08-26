48d29dae74f3aeb51c9a9102e65f5265ead8db64
rafii <rafiiyoussef@gmail.com>
2025-08-26 22:06:43 +0100

    Initial Commit

---
+++ apps/backend/src/main/java/com/accedo/mticket/MticketApplication.java
@@ -0,0 +1,13 @@
+package com.accedo.mticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MticketApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/com/accedo/mticket/MticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accedo.mticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
